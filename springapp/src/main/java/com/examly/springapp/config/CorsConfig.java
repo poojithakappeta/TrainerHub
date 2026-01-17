fe9c5dbe7a19f606ac5e3c71c344cf9cbd95537c
poojithakappeta <poojithakappeta@gmail.com>
2026-01-17 07:05:59 +0530

    added netlify link

--- springapp/src/main/java/com/examly/springapp/config/CorsConfig.java
+++ springapp/src/main/java/com/examly/springapp/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins(
+    "http://localhost:4200",
+    "https://soft-chebakia-9ef9ec.netlify.app"
+)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false)
