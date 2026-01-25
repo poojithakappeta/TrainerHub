b317cae9522040642c2a1bc8523f6f9e1b09da28
poojithakappeta <poojithakappeta@gmail.com>
2026-01-25 15:38:36 +0530

    backend issues 6

--- springapp/src/main/java/com/examly/springapp/config/SecurityConfig.java
+++ springapp/src/main/java/com/examly/springapp/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import org.springframework.web.cors.CorsConfigurationSource;
+
 @Configuration
 public class SecurityConfig {
 
